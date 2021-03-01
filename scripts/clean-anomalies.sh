44037d08805557eeeca73556926dfc8473154fe5
Colin Maudry <colin@maudry.com>
2021-03-01 11:50:56 +0100

    Documentation script

--- scripts/clean-anomalies.sh
+++ scripts/clean-anomalies.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Ce script prend des données au format JSON réglementaire et marque les marchés comportant des anomalies avec un array .anomalies.
+
 json="$1"
 
 dir=`dirname "$0"`
