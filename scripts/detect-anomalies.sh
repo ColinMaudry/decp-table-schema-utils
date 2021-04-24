660bfb19251a990f3197d3b74a5f3ed1b24e1a90
Colin Maudry <colin@maudry.com>
2021-04-24 19:56:11 +0200

    Intitialisation en bash (jq), flow principal en python #16

--- scripts/detect-anomalies.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Ce script prend des données au format JSON réglementaire et marque les marchés comportant des anomalies avec un array .anomalies.
-
-json="$1"
-
-dir=`dirname "$0"`
-
-jq -f $dir/detect-anomalies.jq "$json"
--- scripts/flow.py
+++ scripts/flow.py
@@ -1,27 +1,48 @@
 from dataflows import Flow, load, dump_to_path, dump_to_zip, printer, add_metadata
-# from dataflows import sort_rows, filter_rows, find_replace, delete_fields, set_type, validate, unpivot
+from dataflows import sort_rows, filter_rows, find_replace, delete_fields, set_type, validate, unpivot
 import re
 import ast
 import simplejson as json
+import pandas as pd
+import pyjq as jq
+import subprocess
+
+dataaa = [
+    {
+        'id': 1
+    }
+]
+
+# def action_package(package) :
+#     print(package.pkg.descriptor["resources"][0])
+
+# def donnees_actuelles(rows) :
+#     print(rows)
+#     return rows
+
 
 def simple_json():
     flow = Flow(
-        # Load inputs
-        load('tmp/withAnomalies.json', format = 'json', property = 'marches'),
-        clean_data
+        load("json.csv"),
+        set_type("donneesActuelles", type="boolean"),
+        set_type("acheteur.id", type="string"),
+        set_type("titulaire.id", type="string"),
+        set_type("codeCPV", type="string"),
+        set_type("lieuExecution.code", type="string"),
+        # donnees_actuelles,
+        sort_rows('rootId', resources = 0, reverse = True),
+        dump_to_path("decp")
 
-        # Process them (if necessary)
-        # Save the results
-        # add_metadata(name='simple_json', title='''simple.json''')
     )
     flow.process()
 
-def clean_data(row) :
-    if row["objet"] :
-        row["objet"] = re.sub(' +', ' ', row["objet"].replace("\\n"," "))
 
 # def detect_anomalies(row) :
 
+def json_to_csv() :
+     subprocess.call(['scripts/decpJson-to-csv.sh','decp.json'])
+
 
 if __name__ == '__main__':
+    json_to_csv()
     simple_json()
