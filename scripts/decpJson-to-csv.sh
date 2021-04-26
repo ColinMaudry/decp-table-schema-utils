860e37a222aaca2470c933a66b34b639e8255999
Colin Maudry <colin@maudry.com>
2021-04-26 09:51:25 +0200

    Publication vers sqlite + datasette metadata #16

--- scripts/decpJson-to-csv.sh
+++ scripts/decpJson-to-csv.sh
@@ -5,6 +5,6 @@ dir=`dirname "$0"`
 json=$1
 
 #head -n 1 $dir/../exemples/exemple-valide.csv
-echo "id,rootId,seq,uid,acheteur.id,acheteur.nom,nature,objet,codeCPV,procedure,lieuExecution.code,lieuExecution.typeCode,lieuExecution.nom,dureeMois,dateNotification,datePublicationDonnees,montant,formePrix,titulaire.id,titulaire.typeIdentifiant,titulaire.denominationSociale,objetModification,donneesActuelles,anomalies" > json.csv
+echo "id,rootId,seq,uid,acheteur.id,acheteur.nom,nature,objet,codeCPV,procedure,lieuExecution.code,lieuExecution.typeCode,lieuExecution.nom,dureeMois,dateNotification,datePublicationDonnees,montant,formePrix,titulaire.id,titulaire.typeIdentifiant,titulaire.denominationSociale,objetModification,donneesActuelles,anomalies" > decp.csv
 
-jq -f $dir/detect-anomalies.jq $json | jq -f $dir/decp-to-csv.jq  | jq -r '@csv' >> json.csv
+jq -f $dir/detect-anomalies.jq $json | jq -f $dir/decp-to-csv.jq  | jq -r '@csv' >> decp.csv
--- scripts/flow.py
+++ scripts/flow.py
@@ -1,34 +1,36 @@
-from dataflows import Flow, load, dump_to_path, dump_to_zip, printer, add_metadata
+from dataflows import Flow, load, dump_to_path, dump_to_zip, printer, add_metadata, update_resource
 from dataflows import sort_rows, filter_rows, find_replace, delete_fields, set_type, validate, unpivot
-
+import sqlite3 as sqlite
 import subprocess
+import pandas as pd
+from pathlib import Path
+from datapackage_to_datasette import datapackage_to_datasette, DataImportError
 
 def decp_processing():
     flow = Flow(
-        load("json.csv"),
+        load("decp.csv"),
         set_type("donneesActuelles", type="boolean"),
         set_type("acheteur.id", type="string"),
         set_type("titulaire.id", type="string"),
         set_type("codeCPV", type="string"),
         set_type("lieuExecution.code", type="string"),
-        # donnees_actuelles,
-        sort_rows('seq', resources = 0, reverse = True),
+        # update_resource(0, '{"name": "decp"}'),
+        sort_rows('{rootId}:{seq}', resources = 0, reverse = True),
         donnees_actuelles,
+        delete_fields(["rootId","seq"], resources= 0, regex=False),
         dump_to_path("decp")
 
     )
     flow.process()
 
 
-# def detect_anomalies(row) :
-
 def json_to_csv() :
      subprocess.call(['scripts/decpJson-to-csv.sh','decp.json'])
 
 def donnees_actuelles(rows) :
-    print()
     prevRootId = ""
     donneesActuelles = False
+    prevSeq = 0
 
     for row in rows :
         if row['rootId'] != prevRootId :
@@ -43,6 +45,18 @@ def donnees_actuelles(rows) :
         row['donneesActuelles'] = donneesActuelles
         yield row
 
+def load_to_sqlite() :
+    try:
+        datapackage_to_datasette(
+            'decp/decp.db',
+            'decp/datapackage.json',
+            'decp/datasette.json',
+            write_mode='replace'
+        )
+    except DataImportError:
+        raise
+
 if __name__ == '__main__':
     json_to_csv()
     decp_processing()
+    load_to_sqlite()
