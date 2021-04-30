eced532bdbccf490a9a2d0b35ec9a9fe1ac902de
Colin Maudry <colin@maudry.com>
2021-04-30 16:38:40 +0200

    Script de récupération des données et création db

---
+++ scripts/datapackage-to-datasette.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from datapackage_to_datasette import datapackage_to_datasette, DataImportError
+import requests
+
+def load_to_sqlite() :
+    try:
+        datapackage_to_datasette(
+            'decp/decp.db',
+            'decp/datapackage.json',
+            '/dev/null',
+            write_mode='replace'
+        )
+    except DataImportError:
+        raise
+
+if __name__ == '__main__':
+
+    csv = requests.get('https://www.data.gouv.fr/fr/datasets/r/8587fe77-fb31-4155-8753-f6a3c5e0f5c9', allow_redirects=True)
+    with open("decp/decp.csv", 'w') as csvfile :
+        csvfile.write(csv.content.decode('utf-8'))
+
+    datapackage = requests.get('https://www.data.gouv.fr/fr/datasets/r/65194f6f-e273-4067-8075-56f072d56baf', allow_redirects=True)
+    with open("decp/datapackage.json", 'w') as datapackagefile :
+        datapackagefile.write(datapackage.content.decode('utf-8'))
+
+    load_to_sqlite()
