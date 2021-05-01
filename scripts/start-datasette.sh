1b70344bd0a0db9282e1fc7e71481804d52c7d3c
Colin Maudry <colin@maudry.com>
2021-05-01 23:45:44 +0200

    Fixed port, moved datasette.json

--- scripts/start-datasette.sh
+++ scripts/start-datasette.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-port=$1
-
 rm -f decp/datapackage.json decp/decp.db
 
 # Get data and make SQLite db
 python3 scripts/datapackage-to-datasette.py
 
-datasette decp/decp.db --template-dir datasette/templates --static static:datasette/static/ --metadata decp/datasette.json  --plugins-dir datasette/plugins --port "$port"
+datasette decp/decp.db --template-dir datasette/templates --static static:datasette/static/ --metadata datasette/datasette.json  --plugins-dir datasette/plugins --port 9090
