3eb2421d5718885c6411b8f10fbc1f2ab9841e33
Colin Maudry <colin@maudry.com>
2021-05-01 22:50:17 +0200

    Récupération des donnés

--- scripts/decpJson-to-csv.sh
+++ scripts/decpJson-to-csv.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+json="decp.json"
+
 dir=`dirname "$0"`
 
-json=$1
+if [[ ! -f $json ]]
+then
+    wget https://www.data.gouv.fr/fr/datasets/r/16962018-5c31-4296-9454-5998585496d2 -O $json
+fi
 
 #head -n 1 $dir/../exemples/exemple-valide.csv
 echo "id,rootId,seq,uid,acheteur.id,acheteur.nom,nature,objet,codeCPV,procedure,lieuExecution.code,lieuExecution.typeCode,lieuExecution.nom,dureeMois,dateNotification,datePublicationDonnees,montant,formePrix,titulaire.id,titulaire.typeIdentifiant,titulaire.denominationSociale,objetModification,donneesActuelles,anomalies" > decp.csv
