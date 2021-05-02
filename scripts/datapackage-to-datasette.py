782b22a44b5f5ec1b91308c2883e222bacfe980d
Colin Maudry <colin@maudry.com>
2021-05-02 07:26:36 +0200

    Plus de logs, création du dossier decp

--- scripts/datapackage-to-datasette.py
+++ scripts/datapackage-to-datasette.py
@@ -16,12 +16,15 @@ def load_to_sqlite() :
 
 if __name__ == '__main__':
 
+    print("Téléchargement de decp.csv...")
     csv = requests.get('https://www.data.gouv.fr/fr/datasets/r/8587fe77-fb31-4155-8753-f6a3c5e0f5c9', allow_redirects=True)
     with open("decp/decp.csv", 'w') as csvfile :
         csvfile.write(csv.content.decode('utf-8'))
 
+    print("Téléchargement de datapackage.json...")
     datapackage = requests.get('https://www.data.gouv.fr/fr/datasets/r/65194f6f-e273-4067-8075-56f072d56baf', allow_redirects=True)
     with open("decp/datapackage.json", 'w') as datapackagefile :
         datapackagefile.write(datapackage.content.decode('utf-8'))
 
+    print("Création de la base sqlite...")
     load_to_sqlite()
--- scripts/start-datasette.sh
+++ scripts/start-datasette.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+ls -l
+
+if [[ ! -d decp ]]
+then
+    mkdir decp
+fi
 
 rm -f decp/datapackage.json decp/decp.db
 
-# Get data and make SQLite db
+echo "Get data and make SQLite db..."
 python3 scripts/datapackage-to-datasette.py
 
+echo "Starting datasette..."
 datasette decp/decp.db --template-dir datasette/templates --static static:datasette/static/ --metadata datasette/datasette.json  --plugins-dir datasette/plugins --port 9090
