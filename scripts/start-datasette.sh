ff3ab7acc55cb429382b991393bad0717daaad78
Colin Maudry <colin@maudry.com>
2021-05-02 17:42:38 +0200

    Pas de suppression des fichier dans start-datasette.sh

--- scripts/start-datasette.sh
+++ scripts/start-datasette.sh
@@ -6,10 +6,8 @@ then
     mkdir decp
 fi
 
-rm -f decp/datapackage.json decp/decp.db
-
 echo "Get data and make SQLite db..."
 python3 scripts/datapackage-to-datasette.py
 
 echo "Starting datasette..."
-datasette decp/decp.db --template-dir datasette/templates --static static:datasette/static/ --metadata datasette/datasette.json  --plugins-dir datasette/plugins --port 9090
+datasette decp/db.sqlite --template-dir datasette/templates --static static:datasette/static/ --metadata datasette/datasette.json  --plugins-dir datasette/plugins --port 9090
