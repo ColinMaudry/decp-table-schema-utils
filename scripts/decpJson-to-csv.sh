b1e06b5bb9ea6c2a1bd66fee33675f02273e1784
Colin Maudry <colin@maudry.com>
2021-05-03 14:59:17 +0200

    Ajout de 'source' dans le header #8

--- scripts/decpJson-to-csv.sh
+++ scripts/decpJson-to-csv.sh
@@ -10,7 +10,7 @@ then
 fi
 
 #head -n 1 $dir/../exemples/exemple-valide.csv
-echo "id,rootId,seq,uid,acheteur.id,acheteur.nom,nature,objet,codeCPV,procedure,lieuExecution.code,lieuExecution.typeCode,lieuExecution.nom,dureeMois,dateNotification,datePublicationDonnees,montant,formePrix,titulaire.id,titulaire.typeIdentifiant,titulaire.denominationSociale,objetModification,donneesActuelles,anomalies" > decp.csv
+echo "id,rootId,seq,uid,acheteur.id,acheteur.nom,nature,objet,codeCPV,procedure,lieuExecution.code,lieuExecution.typeCode,lieuExecution.nom,dureeMois,dateNotification,datePublicationDonnees,montant,formePrix,titulaire.id,titulaire.typeIdentifiant,titulaire.denominationSociale,objetModification,source,donneesActuelles,anomalies" > decp.csv
 
 now=`date +"%Y-%m-%dT%H:%M:%SZ"`
 jq -f $dir/detect-anomalies.jq $json | jq -f $dir/decp-to-csv.jq --arg now "$now"  | jq -r '@csv' >> decp.csv
