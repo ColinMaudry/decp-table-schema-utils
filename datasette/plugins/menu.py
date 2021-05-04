881cb560f475c2d92d7a2def4fc58e30fdbd4fb5
Colin Maudry <colin@maudry.com>
2021-05-04 11:15:11 +0200

    Màj menu, index et footer

--- datasette/plugins/menu.py
+++ datasette/plugins/menu.py
@@ -3,6 +3,7 @@ from datasette import hookimpl
 @hookimpl
 def menu_links(datasette, actor):
     return [
+        {"href": "https://teamopendata.org/t/decp-info-les-donnees-de-la-commande-publique-pour-tous-questions-reponses-discussions/2948", "label": "Présentation / FAQ"},
         {"href": "https://github.com/ColinMaudry/decp-table-schema", "label": "Documentation des champs"},
         {"href": "https://github.com/ColinMaudry/decp-table-schema-utils", "label": "Code source"}
     ]
