cb006a161ea2cd96bd5d78f058af582f4a88b8d2
Colin Maudry <colin@maudry.com>
2021-05-18 11:29:51 +0200

    feat: ajout des données précédentes et concaténation (failing)

--- scripts/flow.py
+++ scripts/flow.py
@@ -1,24 +1,42 @@
-from dataflows import Flow, load, dump_to_path, dump_to_zip, printer, add_metadata, update_resource
-from dataflows import sort_rows, delete_fields, set_type, duplicate, deduplicate, unpivot, set_primary_key
+from dataflows import Flow, load, dump_to_path, sort_rows, delete_fields, set_type, duplicate, deduplicate, set_primary_key, concatenate
 import subprocess
 from datapackage_to_datasette import datapackage_to_datasette, DataImportError
 
 def decp_processing():
     flow = Flow(
+
+    # Chargement du CSV suite à la conversion depuis JSON
         load("decp.csv"),
         set_type("acheteur.id", type="string"),
         set_type("titulaire.id", type="string"),
         set_type("codeCPV", type="string"),
         set_type("lieuExecution.code", type="string"),
-        # update_resource(0, '{"name": "decp"}'),
+
+    # Tri par rootId et seq pour préparer à la création de donneesActuelles
         sort_rows('{rootId}:{seq}', resources = 0, reverse = True),
         donnees_actuelles,
+
+    # rootId et seq peuvent maintenant être supprimés
         delete_fields(["rootId","seq"], resources= 0, regex = False),
         sort_rows('{datePublicationDonnees}', resources = 0, reverse = True),
+
+    # Nouvelle table dédiée aux marchés, sans données sur les titulaires
         duplicate(source="decp", target_name="sans-titulaires", target_path="sans-titulaires.csv", duplicate_to_end=True),
         delete_fields(["titulaire.id","titulaire.denominationSociale"], resources = "sans-titulaires", regex = False),
-        set_primary_key(["uid"], resources = 1),
+        set_primary_key(["uid"], resources = "sans-titulaires"),
         deduplicate(),
+
+    # Chargement des précédentes DECP au format CSV, pour extraction des nouvelles données
+        print("Téléchargement des données tabulaires précédentes..."),
+        load("https://decp.info/db/decp.csv?_size=max&_dl=1", name="previous-decp"),
+        set_type("acheteur.id", type="string"),
+        set_type("titulaire.id", type="string"),
+        set_type("codeCPV", type="string"),
+        set_type("lieuExecution.code", type="string"),
+        delete_fields(["rowid"], resources="previous-decp", regex=False),
+        #print("Fusion des données tabulaires précédentes et des données d'aujourd'hui..."),
+        concatenate({},target={"name": "decp-titulaires","path": "decp-titulaires.csv"},resources=["decp","previous-decp"]),
+        #print("Enregistrement des données sur le disque..."),
         dump_to_path("decp")
 
 
@@ -62,7 +80,7 @@ def load_to_sqlite() :
 
 if __name__ == '__main__':
     print("Conversion JSON vers CSV, et détection des anomalies...")
-    json_to_csv()
+    #json_to_csv()
     print("Chargement du CSV, tri...")
     decp_processing()
     print("Chargement dans SQLite et création des métadonnées datasette...")
