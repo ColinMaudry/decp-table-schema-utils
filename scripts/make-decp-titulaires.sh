043b1945ddb4d1cdf5560a387e5e69e43d714c82
Colin Maudry <colin@maudry.com>
2021-05-20 10:34:40 +0200

    feat: correction et optimisation du script de génération du fichier titulaires

--- scripts/make-decp-titulaires.sh
+++ scripts/make-decp-titulaires.sh
@@ -11,45 +11,67 @@
 #
 # wait
 
+echo "Extraction des SIRET des DECP..."
+
+echo "titulaire.id,titulaire.typeIdentifiant" > decpSirets.csv
+xsv select titulaire.id,titulaire.typeIdentifiant decp/decp.csv | tail -n +2 | grep "SIRET" | sort -u >> decpSirets.csv
+
+echo "Récupération des données SIRET..."
+
+xsv join --left titulaire.id decpSirets.csv siret StockEtablissement_utf8_geo.csv > decpSirets-full-etab.csv
+
+echo "Récupération des données SIREN..."
+
+xsv join --left siren decpSirets-full-etab.csv siren StockUniteLegale_utf8.csv > decpSirets-full.csv
 
 echo "Selection des colonnes du fichier SIRET..."
 
-echo "siret,siren,longitude,latitude,codeAPE,etatEtablissement,departement" > siret-selection.csv
+echo "siret,siren,longitude,latitude,codeAPE,departement" > siret-selection.csv
 
-xsv select siret,siren,longitude,latitude,activitePrincipaleEtablissement,codeCommuneEtablissement StockEtablissement_utf8_geo.csv | tail -n +2 | sed -E 's/([0-9][0-9])[0-9][0-9][0-9]$/\1/' >> siret-selection.csv &
+xsv select siret,siren,longitude,latitude,activitePrincipaleEtablissement,codeCommuneEtablissement decpSirets-full.csv | tail -n +2 | sed -E 's/([0-9][0-9])[0-9][0-9][0-9]$/\1/' >> siret-selection.csv &
 
-xsv select siret,etatAdministratifEtablissement StockEtablissement_utf8_geo.csv | sed 's/A$/Actif/' | sed 's/F$/Fermé/' > etatEtablissement.csv &
+echo "siret,etatEtablissement" > etatEtablissement.csv
+xsv select siret,etatAdministratifEtablissement decpSirets-full.csv | tail -n +2 | sed 's/A$/Actif/' | sed 's/F$/Fermé/' >> etatEtablissement.csv &
 
 echo "Selection des colonnes du fichier SIREN..."
 
 echo "siren,categorie" > siren-selection.csv
+xsv select siren,categorieEntreprise decpSirets-full.csv  | tail -n +2 >> siren-selection.csv &
 
-xsv select siren,categorieEntreprise StockUniteLegale_utf8.csv | tail -n +2 >> siren-selection.csv &
+echo "Récupération et correction des données ESS et état du fichier SIREN..."
 
-xsv select siren,etatAdministratifUniteLegale StockUniteLegale_utf8.csv | sed 's/A$/Active/' | sed 's/C$/Cessée/' > etatEntreprise.csv &
+echo "siren,etatEntreprise,ESS" > etatEssEntreprise.csv
+xsv select siren,etatAdministratifUniteLegale,economieSocialeSolidaireUniteLegale decpSirets-full.csv | tail -n +2 | sed 's/,A,/,Active,/' | sed 's/,C,/,Cessée,/' | sed 's/O$/oui/' | sed 's/N$/non/' >> etatEssEntreprise.csv &
+
+
+echo "Récupération et raccourcissement des categories juridiques du fichier SIREN..."
+
+echo "siren,categorieJuridique" > cjEntreprise.csv
+xsv select siren,categorieJuridiqueUniteLegale decpSirets-full.csv | tail -n +2 | sed -E 's/([0-9][0-9])[0-9][0-9]$/\1/'  >> cjEntreprise.csv
 
 wait
 
-echo "Récupération des libellés des categories juridiques..."
-
-echo "siren,categorieJuridique" > cjEntreprise.csv
-
-xsv select siren,categorieJuridiqueUniteLegale StockUniteLegale_utf8.csv | sed -E 's/([0-9][0-9])[0-9][0-9]$/\1/'  >> cjEntreprise.csv
+echo "Récupération des libellés des catégories juridiques..."
 
 xsv join --left categorieJuridique cjEntreprise.csv code cj.csv > cjEntreprise-libelle.csv
 
-xsv join --left siren siren-selection.csv siren cjEntreprise-libelle.csv > siren-selection-cj.csv
+echo "Fusion des données siren et des catégories juridiques..."
 
-echo "Fusion des données établissements et unités légale..."
+xsv cat columns siren-selection.csv cjEntreprise-libelle.csv > siren-selection-cj.csv
 
-xsv join --left siren siren-selection-cj.csv siren etatEntreprise.csv > siren-selection-etat-cj.csv &
+echo "Fusion des données siren+cj et etat+ess+entreprise..."
 
-xsv join --left siret siret-selection.csv siret etatEtablissement.csv > siret-selection-etat.csv &
+xsv cat columns siren-selection-cj.csv etatEssEntreprise.csv > siren-selection-etat-cj.csv &
+
+echo "Fusion des données siret et etat..."
+
+xsv cat columns siret-selection.csv etatEtablissement.csv > siret-selection-etat.csv &
 
 wait
 
 # rm siren-selection.csv siret-selection.csv etatEntreprise.csv etatEtablissement.csv
 
+echo "Fusion des données siret et siren..."
 
 xsv join --left siren siret-selection-etat.csv siren siren-selection-etat-cj.csv | xsv select siret,codeAPE,departement,categorie,categorieJuridique,categorieJuridiqueLibelle1,categorieJuridiqueLibelle2,etatEtablissement,etatEntreprise,longitude,latitude > siren-siret.csv &
 
@@ -61,4 +83,4 @@ wait
 
 echo "Fusion des DECP et des données SIRENE..."
 
-xsv join --left "titulaire.id" decp-selection.csv "siret" siren-siret.csv > decp-titulaires.csv
+xsv join --left "titulaire.id" decp-selection.csv "siret" siren-siret.csv | xsv select id,uid,acheteur.id,acheteur.nom,nature,objet,codeCPV,lieuExecution.code,lieuExecution.typeCode,lieuExecution.nom,dureeMois,dateNotification,montant,titulaire.id,titulaire.typeIdentifiant,titulaire.denominationSociale,codeAPE,departement,categorie,categorieJuridique,categorieJuridiqueLibelle1,categorieJuridiqueLibelle2,etatEtablissement,etatEntreprise,longitude,latitude,donneesActuelles,anomalies > decp-titulaires.csv
